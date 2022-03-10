ae4353d99d402d70a5b10e8346c6d78e74aae344
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-03-10 02:10:55 -0500

    Modified ex 3 and add Exercise 8

--- Activity2/Exercise3/ex3.sql
+++ Activity2/Exercise3/ex3.sql
@@ -1,6 +1,6 @@
 -- Exercise 3
--- Presente de todos los constructores (Builder) quien tiene casas en construcción,
--- el resultado en una sola columna llamada Full_Name especifica el nombre de estos
+-- Presente de todos los constructores (Builder) que tiene casas en construcción,
+-- el resultado en una sola columna llamada Full_Name, especificar el nombre de estos
 -- constructores que deben estar ordenados por apellido. Asegúrese de que se obtengan 8 registros.
 
 -- Se pueden obtener Builders repetidos, ya que un mismo Builder pudo haber construido más de una casa
